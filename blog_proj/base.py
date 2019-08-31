2fcd10241d1923079e8cf9542e03a440293f1147
sthitaprajnadas <sthitaprajnadas@gmail.com>
2019-08-31 15:30:01 +0530

    all unstaged commit

--- blog_proj/base.py
+++ blog_proj/base.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import Config
 
-
 engine = create_engine('sqlite:///flaskblog.db',echo=True)
 #engine = create_engine(Config.SQLALCHEMY_DATABASE_URI,echo=True)
 Session = sessionmaker(bind=engine)
